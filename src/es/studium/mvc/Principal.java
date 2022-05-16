976581f4ff7a76da76df6d090682905d79d5e355
InmaRiv <conchi.rp94@gmail.com>
2022-05-16 13:58:37 +0200

    First Commit

---
+++ src/es/studium/mvc/Principal.java
@@ -0,0 +1,10 @@
+package es.studium.mvc;
+public class Principal
+{
+	public static void main(String[] args)
+	{
+		MenuPrincipal menuPrincipal = new MenuPrincipal();
+		Modelo modelo = new modelo();
+		new Controlador = (menuPrincipal, modelo);
+	}
+}
