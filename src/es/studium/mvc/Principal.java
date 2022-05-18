10903c5e3e2281d18e2f53b68cad8964f5e7dfa9
fran <carballo1989d@gmail.com>
2022-05-18 09:51:12 +0200

    vista ranking

--- src/es/studium/mvc/Principal.java
+++ src/es/studium/mvc/Principal.java
@@ -2,9 +2,11 @@ package es.studium.mvc;
 public class Principal
 {
 	public static void main(String[] args)
+	
 	{
-		MenuPrincipal menuPrincipal = new MenuPrincipal();
-		Modelo modelo = new modelo();
-		new Controlador = (menuPrincipal, modelo);
+		new Ranking();
+		//MenuPrincipal menuPrincipal = new MenuPrincipal();
+		//Modelo modelo = new modelo();
+		//new Controlador = (menuPrincipal, modelo);
 	}
 }
