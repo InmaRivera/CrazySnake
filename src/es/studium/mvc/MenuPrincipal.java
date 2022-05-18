e4de2775f1fff21efd300f713b4ed21b43e64167
InmaRiv <conchi.rp94@gmail.com>
2022-05-18 08:52:02 +0200

    Vista Menú Principal

---
+++ src/es/studium/mvc/MenuPrincipal.java
@@ -0,0 +1,35 @@
+package es.studium.mvc;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+public class MenuPrincipal extends Frame
+{
+	private static final long serialVersionUID = 1L;
+	Label lblMenu = new Label ("                                 Menú principal");
+	Button btnPartida = new Button("Empezar partida");
+	Button btnTop = new Button("Top 10");
+	Button btnAyuda= new Button("Ayuda");// btnExit
+	Button btnExit= new Button("Exit");
+
+	public MenuPrincipal()
+	{
+		//Configuración Pantalla
+		setSize(300, 220); // Tamaño: Ancho x Alto
+		//setResizable(false); // No permitir redimensión
+		setLayout(new GridLayout(5,1));
+		
+		setBackground(new Color(255, 255, 153));
+		setTitle("Menú Principal");
+		add(lblMenu);
+		add(btnPartida);
+		add(btnTop);
+		add(btnAyuda);
+		add(btnExit);
+		
+		setVisible(true);
+		setSize(299, 246); // En pixeles Width, Height. Ir probando y cambiando anchura y altura
+		setLocationRelativeTo(null); // Centrar
+	}
+}
--- src/es/studium/mvc/Principal.java
+++ src/es/studium/mvc/Principal.java
@@ -4,7 +4,7 @@ public class Principal
 	public static void main(String[] args)
 	{
 		MenuPrincipal menuPrincipal = new MenuPrincipal();
-		Modelo modelo = new modelo();
+		Modelo modelo = new Modelo();
 		new Controlador = (menuPrincipal, modelo);
 	}
 }
