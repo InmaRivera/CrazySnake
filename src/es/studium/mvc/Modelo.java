dfe60899a31e48c2066127d615cfdaf10b1e8093
fran <carballo1989d@gmail.com>
2022-06-11 11:45:58 +0200

    Tablero

--- src/es/studium/mvc/Modelo.java
+++ src/es/studium/mvc/Modelo.java
@@ -18,7 +18,7 @@ public class Modelo
 	String password = "Studium2021;";
 
 	// Creamos una consulta a la base de datos
-	String sentencia = "SELECT nombreJugador, puntosJugador FROM jugadores where puntosJugador order by 2 desc;" ; 
+	String sentencia = "SELECT nombreJugador puntosJugador FROM jugadores where puntosJugador order by 2 desc;" ; 
 	
 	 
 	// Objeto donde se guarda la información de la consulta a la base de datos
